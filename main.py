51fa208782aad8d7cead3cb593ab60a0e10ddbe1
ARand0mCoder <d.leijnse@gmail.com>
2025-11-12 11:25:32 +0100

    change f and work on getting spherical instead of cubic mesh

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ import math as m
 from skimage import measure
 
 def f(x, y, z):
-    return 0.4 + 0.7 * (x**2 + y**2 + z**2) + 1.6 * x*y*z - 1.3 *  (x**4 + y**4 + z**4)
+    return 0.4 - 0.7 * (x**2 + y**2 + z**2) + 1.6 * x*y*z - 1.3 *  (x**4 + y**4 + z**4) + 1.3 * (x**2 * y**2 + x**2 * z**2 + y**2 * z**2)
+
+def dist2(x, y, z):
+    # return the square of the distance
+    return x ** 2 + y ** 2 + z ** 2
 
 def fpt(pt):
     x = pt[0]
     y = pt[1]
+    
     z = pt[2]
     return f(x, y, z)
 
@@ -19,7 +24,7 @@ def plot_scatter():
     delta = 0.05 # step size for computation
     epsilon = 0.001 # we consider a point to be a zero of f if f(x,y,z) < epsilon
             # TODO: maybe let epsilon depend on the distance from the origin
-    bound = 3 # we consider x,y,z all between -bound and bound
+    bound = 1 # we consider x,y,z all between -bound and bound
     steps = m.ceil(bound * 2 / delta)
 
     X = np.arange(-bound, bound, delta)
@@ -43,15 +48,43 @@ def plot_scatter():
 
     plt.show()
 
-def plot_marching():
-    delta = 0.05
+def plot_marching(circular=False):
+    delta = 0.5
     bound = 1
     steps = m.ceil(bound * 2 / delta)
     xs = np.linspace(-bound, bound, steps)
     X, Y, Z = np.meshgrid(xs, xs, xs)
     F = f(X, Y, Z)
-    verts, faces, normals, values = measure.marching_cubes(F, 0, spacing=[np.diff(xs)[0]]*3)
+
+    if (circular):
+        M = 0.3
+        # We set F(X, Y, Z) to the value of M if the coordinat point has radius > bound.
+        D = dist2(X, Y, Z)
+        # print(D <= bound)
+        print(F)
+        F2 = np.where(D <= (bound ** 2), F, M)
+        print(F2)
+    else:
+        F2 = F
+
+    if False:
+        vX = np.transpose(verts)[0]
+        vY = np.transpose(verts)[1]
+        vZ = np.transpose(verts)[2]
+        D = dist2(vX, vY, vZ)
+        verts = verts[D <= (bound ** 2)]
+
+        NumVerts = len(verts)
+        facesX = np.transpose(faces)[0]
+        facesY = np.transpose(faces)[1]
+        facesZ = np.transpose(faces)[2]
+        M = np.maximum(facesX, np.maximum(facesY, facesZ))   
+        faces = faces[M < NumVerts]
+        
+    verts, faces, normals, values = measure.marching_cubes(F2, 0, spacing=[np.diff(xs)[0]]*3)
     verts -= 3
+
+    
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -59,4 +92,4 @@ def plot_marching():
     plt.show()
 
 
-plot_marching()
+plot_marching(circular=True)
